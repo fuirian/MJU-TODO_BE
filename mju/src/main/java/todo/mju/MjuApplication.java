0ecec2bc7cf95fc2df101c207f8494c59a730114
김민진 <128349855+fuirian@users.noreply.github.com>
2024-07-25 22:39:16 +0900

    Add files via upload

---
+++ mju/src/main/java/todo/mju/MjuApplication.java
@@ -0,0 +1,13 @@
+package todo.mju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjuApplication.class, args);
+	}
+
+}
---
+++ mju/src/test/java/todo/mju/MjuApplicationTests.java
@@ -0,0 +1,13 @@
+package todo.mju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
